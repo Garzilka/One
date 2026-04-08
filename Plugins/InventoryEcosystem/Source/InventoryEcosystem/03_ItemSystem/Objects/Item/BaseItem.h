0f459de9d4e5314e2e22b6187240ed8cf0ca18ae
Nomads <Gruzders@yandex.ru>
2026-04-08 11:37:04 +0300

    [А.Поляков] Система предметов
    - Добавлена основа предметов

---
+++ Plugins/InventoryEcosystem/Source/InventoryEcosystem/03_ItemSystem/Objects/Item/BaseItem.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "InventoryEcosystem/01_Core/Object/BaseInventoryObject.h"
+#include "BaseItem.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FItemUpdateSignature, class UBaseItem*, Item);
+
+UCLASS(Abstract, BlueprintType, Blueprintable)
+class ITEMSYSTEM_API UBaseItem : public UBaseInventoryObject
+{
+	GENERATED_BODY()
+	
+public:
+
+};
